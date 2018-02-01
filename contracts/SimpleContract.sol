06340708d296ab99915a093b2fb5692cd970a547
Jens Laufer <jenslaufer@gmail.com>
2018-02-01 17:09:42 +0100

    added self destruct call

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -1,10 +1,16 @@
 pragma solidity ^0.4.2;
 
 contract SimpleContract {
-	address public owner;
+	address  owner;
     
-    function SimpleContract() public{
+    function SimpleContract() public {
     	owner = msg.sender;
+    }
+
+    function kill() {
+       if (owner == msg.sender) { // We check who is calling
+          selfdestruct(owner); //Destruct the contract
+       }
     }
 
     function helloworld() public pure returns(string){
